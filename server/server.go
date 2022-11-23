93e714487a85f99b5f64842e868a7ff449944c4d
Mikkel Bistrup <mikkel@bistrup.dk>
2022-11-23 16:26:43 +0100

    added OpenServer function to server.go

--- server/server.go
+++ server/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
+	"net"
 	"time"
+
+	"google.golang.org/grpc"
 )
 
 const (
@@ -14,4 +18,29 @@ func main() {
 
 type Server struct {
 	services.UnimplementedServiceServer
+}
+
+func OpenServer() {
+	log.Print("Loading...")
+
+	listener, err := net.Listen("tcp", "localhost:5000")
+
+	if err != nil {
+		log.Fatalf("Could not listen @ %s", err)
+		return
+	}
+
+	log.Print("Server is setup at port 5000.")
+
+	s := services.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	services.RegisterServicesServer(grpcServer, &s)
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatal("Failed to start gRPC Server :: %v", err)
+	}
 }
