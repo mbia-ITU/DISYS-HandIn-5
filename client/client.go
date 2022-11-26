9b3aa7c93fa3ae498da16fa571c3a270f80c5ff9
Mikkel Bistrup <mikkel@bistrup.dk>
2022-11-27 00:03:01 +0100

    Finished server.go and began on client.go

--- client/client.go
+++ client/client.go
@@ -1,1 +1,108 @@
 package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+
+	/*
+		"sync"
+		"time"
+		"context"
+		"fmt"
+	*/
+
+	service "github.com/mbia-ITU/DISYS-HandIn-5/gRPC/gRPC"
+	/*
+		"google.golang.org/grpc"
+		"google.golang.org/protobuf/types/known/emptypb"
+	*/)
+
+var (
+	bidderName  string
+	rmDirectory = make(map[string]replicationManager)
+)
+
+type replicationManager struct {
+	serviceClient service.ThisserviceClient
+	address       string
+}
+
+func main() {
+
+}
+
+func getReplicationManagers() {
+
+}
+
+func auctionManager() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		replicationManagers = getReplicationManagers
+
+		result, err := getHighestBid(&replicationManagers)
+		if err != nil {
+			log.Printf("Trouble fetching result from replication managers %d\n", err)
+			continue
+		}
+
+		if scanner.Text() == "status" {
+			if result.getStatus == service.Status_AUCTION_OVER {
+				log.Printf("Auction is over. The winning bid was %v made by %v\n", result.Amount, result.Bidder)
+				return
+			} else {
+				log.Printf("Current highest bid is %v made by %v.\n", result.Amount, result.Bidder)
+				continue
+			}
+
+		}
+
+		bid, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Printf("Input %v could not be converted to integer: %v\n", bid, err)
+		}
+
+		if result.getStatus() == service.Status_AUCTION_OVER {
+			log.Printf("Auction is over. The winning bid was %v made by %v\n", result.Amount, result.Bidder)
+			return
+		}
+
+		if bid <= result.GetAmount {
+			log.Printf("Tried to bid %v, but bid was to low compared to the current highest bid of %v made by %v\n", bid, result.Amount, result.Bidder)
+			continue
+		} else {
+			log.Printf("Making a bid of %v. To beat the current highest bid.\n", bid)
+			MakeABidToAllReplications(&replicationManagers, bid)
+		}
+
+	}
+}
+
+func MakeABid(bid int32) error {
+
+	return nil
+
+}
+
+func MakeABidToAllReplications() {
+
+}
+
+func getHighestBid() {
+
+}
+
+func GetResult() {
+
+}
+
+func connectToNode() {
+
+}
+
+func connectToAllNodes() {
+
+}
--- server/server.go
+++ server/server.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"sync"
 	"time"
 
 	service "github.com/mbia-ITU/DISYS-HandIn-5/gRPC/gRPC"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 const (
 	auctionTime = 60 * time.Second
 )
 
+var (
+	highestbid = service.Result{
+		Bidder: "No one",
+		Amount: 0,
+		Status: service.Status_SUCCESS,
+	}
+
+	Locker sync.Mutex
+)
+
 func main() {
+	go func() {
+		time.Sleep(auctionTime)
+		auctionOver()
+
+		time.Sleep(3 * time.Second)
+	}()
 
 	OpenServer()
 }
@@ -24,14 +45,16 @@ type Server struct {
 
 func OpenServer() {
 	log.Print("Loading...")
+	port := os.Getenv("PORT")
+	address := fmt.Sprintf("localhost:%v", port)
 
-	listener, err := net.Listen("tcp", "localhost:5000")
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("Error while attempting to listen on port 5000: %v", err)
+		log.Fatalf("Error while attempting to listen on port %v: %v", port, err)
 		return
 	}
 
-	log.Print("Server is setup at port 5000.")
+	log.Print("Server is setup at port %v.", port)
 	grpcServer := grpc.NewServer()
 
 	server := Server{}
@@ -40,4 +63,43 @@ func OpenServer() {
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("Failed to start gRPC Server :: %v", err)
 	}
+}
+
+func (s *Server) MakeABid(ctx context.Context, bid *service.Bid) (*service.Result, error) {
+	Locker.Lock()
+	defer Locker.Unlock()
+
+	if highestbid.Status == service.Status_AUCTION_OVER {
+		log.Printf("Tried to make a bid of %v by %v, but auction was closed.\n", bid.Amount, bid.Uid)
+		return &service.Result{Status: service.Status_AUCTION_OVER}, nil
+	}
+
+	if bid.Amount <= highestbid.Amount {
+		log.Printf("Tried to bid %v by %v, but bid was to low compared to the current highest bid of %v made by %v\n", bid.Amount, bid.Uid, highestbid.Amount, highestbid.Bidder)
+		return &service.Result{Status: service.Status_TOO_LOW}, nil
+	}
+
+	highestbid = service.Result{
+		Amount: bid.Amount,
+		Bidder: bid.Uid,
+		Status: service.Status_SUCCESS,
+	}
+	log.Printf("New highest bid of %v made by %v has beaten old bid of %v by %v\n", bid.Amount, bid.Uid, highestbid.Amount, highestbid.Bidder)
+
+	return &highestbid, nil
+}
+
+func (s *Server) GetResult(ctx context.Context, _ *emptypb.Empty) (*service.Result, error) {
+	Locker.Lock()
+	defer Locker.Unlock()
+	log.Printf("Highest bid is %v made by %v\n", highestbid.Amount, highestbid.Bidder)
+	return &highestbid, nil
+}
+
+func auctionOver() {
+	Locker.Lock()
+	defer Locker.Unlock()
+
+	highestbid.Status = service.Status_AUCTION_OVER
+	log.Printf("Auction is over. The winning bid was %v made by %v\n", highestbid.Amount, highestbid.Bidder)
 }
